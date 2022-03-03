583ca1eeaa4ce6a47166ce61c1ff5a2805178450
annabrisland <84166964+annabrisland@users.noreply.github.com>
2022-03-03 13:14:28 -0800

    Update import.py
    Reads in czi image

--- import.py
+++ import.py
@@ -2,3 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import measure, color, io
+import czifile
+
+image = czifile.imread('H99_48hrs-04.czi')
+print(image.shape)
