fa1b89ec053adb7e832e17e07ca60781f133b087
annabrisland <84166964+annabrisland@users.noreply.github.com>
2022-03-08 16:43:58 -0800

    Update importAndReadCzi.py
    Enable reading and plotting of .czi

--- importAndReadCzi.py
+++ importAndReadCzi.py
@@ -3,6 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from skimage import measure, color, io
 import czifile
+#from aicsimageio import AICSImage
+#from czifile import czi2tif
+
 
 image = czifile.imread('H99_48hrs-04.czi')
-print(image.shape)
+
+cells = image[0,0,:,:,:]
+
+plt.imshow(cells, cmap = 'gray')
